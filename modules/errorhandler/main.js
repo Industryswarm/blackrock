1b79ca3c65722330523616a9f71066d25b453b26
Darren Smith <contact@darrensmith.com.au>
2020-09-19 10:10:44 +1000

    Made ErrorHandler reactive

--- modules/errorhandler/main.js
+++ modules/errorhandler/main.js
@@ -7,26 +7,93 @@
 
 ;!function(undefined) {
 
+
+
+
+
 	/** Create parent event emitter object from which to inherit ismod object */
-	String.prototype.endsWith = function(suffix) {return this.indexOf(suffix, this.length - suffix.length) !== -1;};
-	var isnode, ismod, log;
+	var isnode, ismod, log, errorCount = 0, errorMessages = {}, pipelines = {}, streamFns = {}, lib, rx, op, Observable;
+
+
+
+
+
+
+
+	/**
+	 * =============================
+	 * Logger Initialisation Methods
+	 * =============================
+	 */
 
 	/**
 	 * (Constructor) Initialises the module
-	 * @param {object} isnodeObj - The parent isnode object
+	 * @param {object} isnode - The parent isnode object
 	 */
 	var init = function(isnodeObj){
-		isnode = isnodeObj, ismod = new isnode.ISMod("ErrorHandler"), log = isnode.module("logger").log;
-		manageErrors();
+		isnode = isnodeObj, ismod = new isnode.ISMod("ErrorHandler"), ismod.log = log = isnode.module("logger").log;
+		lib = isnode.lib, rx = lib.rxjs, op = lib.operators, Observable = rx.Observable;
+		var ISPipeline = pipelines.setupErrorHandler();
+		new ISPipeline({}).pipe();
 		return ismod;
 	}
 
+
+
+
+
+
 	/**
-	 * (Internal) Manages uncaught exceptions
+	 * =====================
+	 * Event Stream Pipeline
+	 * =====================
 	 */
-	var manageErrors = function(){
-		var errorCount = 0;
-		var errorMessages = {};
+
+
+	/**
+	 * (Internal > Pipeline [1]) Setup Error Handler
+	 */
+	pipelines.setupErrorHandler = function(){
+		return new isnode.ISNode().extend({
+			constructor: function(evt) { this.evt = evt; },
+			callback: function(cb) { return cb(this.evt); },
+			pipe: function() {
+				const self = this; const stream = rx.bindCallback((cb) => {self.callback(cb);})();
+				const stream1 = stream.pipe(
+
+					// Fires once on server initialisation:
+					op.map(evt => { if(evt) return streamFns.setupErrorHandled(evt); }),
+					op.map(evt => { if(evt) return streamFns.setupUncaughtException(evt); }),
+					
+				);
+				stream1.subscribe(function(res) {
+					//console.log(res);
+				});
+			}
+		});
+	};
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * =========================================
+	 * Error Handler Stream Processing Functions
+	 * (Fires Once on Server Initialisation)
+	 * =========================================
+	 */
+
+	/**
+	 * (Internal > Stream Methods [1]) Setup Error Handled
+	 * @param {object} evt - The Request Event
+	 */
+	streamFns.setupErrorHandled = function(evt){
 		ismod.on("errorhandled", function(err){
 			if(errorMessages[err] && errorMessages[err] == true){
 				errorCount --;
@@ -35,6 +102,14 @@
 				return;
 			}
 		});
+		return evt;
+	}
+
+	/**
+	 * (Internal > Stream Methods [2]) Setup Uncaught Exception
+	 * @param {object} evt - The Request Event
+	 */
+	streamFns.setupUncaughtException = function(evt){
 		process.on('uncaughtException', function(err) {
 			errorMessages[err.message] = true;
 			errorCount ++;
@@ -56,7 +131,15 @@
 				counter += 10;
 			}, 10);
 		});
+		return evt;
 	}
+
+
+
+
+
+
+
 
 	/**
 	 * (Internal) Export Module
